0943abb5b5ac09ffa1da19bc8b989f6a7275730b
SEMINA <bsewmina@gmail.com>
2021-05-21 09:32:58 +0530

    update v4

--- src/main/java/org/TeamCipher/AddSessionController.java
+++ src/main/java/org/TeamCipher/AddSessionController.java
@@ -2,20 +2,24 @@ package org.TeamCipher;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
-public class AddSessionController {
+public class AddSessionController implements Initializable{
 
-    //------------------------------------------------------------------------------------------
-
+    public TextField txtField_lecturers;
+    public ChoiceBox choBox_seleLec1,choBox_seleLec2;
     @FXML
     private Label mainLabel;
-    @FXML
-    private TextField txtField_lecturer1,txtField_lecturer2;
+
 
 
 
@@ -68,5 +72,85 @@ public class AddSessionController {
 
 
     }
+
     //------------------------------------------------------------------------------------------
+
+    public void next(ActionEvent event) throws IOException {
+        String check = validate();
+        if (check.equals("true")) {
+            App.setRoot("AddSession2");
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(check);
+            alert.showAndWait();
+            mainLabel.setText(validate());
+        }
+    }
+    public void clear(ActionEvent event) {
+        String isTrue = conform();
+        txtField_lecturers.setText("");
+        choBox_seleLec1.setValue(null);
+        choBox_seleLec2.setValue(null);
+    }
+
+    public void getLecture1() throws SQLException {
+        Connection con = SQliteConnection.DBconnect();
+        ResultSet rs = null;
+        try {
+            rs = con.createStatement().executeQuery("SELECT * FROM Lecturer");
+            while (rs.next()) {
+                choBox_seleLec1.getItems().add(rs.getString("name"));
+            }
+           // txtField_lecturers.setText((String) choBox_seleLec1.getValue());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void getLecture2() {
+        Connection con = SQliteConnection.DBconnect();
+        ResultSet rs = null;
+        try {
+            rs = con.createStatement().executeQuery("SELECT * FROM Lecturer");
+            while (rs.next()) {
+                choBox_seleLec2.getItems().add(rs.getString("name"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public String validate() {
+        if (choBox_seleLec1.getValue() == null){
+            return "Insert Lecurer";
+        }
+        return  "true";
+    }
+
+    private String conform() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Confirmation");
+        alert.setHeaderText("You are going to delete this entry");
+        alert.setContentText("Are you sure?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            System.out.println("OKAY");
+            return "true";
+        } else {
+            System.out.println("CANCEL");
+            return "false";
+        }
+    }
+
+    public void initialize(URL url, ResourceBundle resources) {
+        try {
+            getLecture1();
+            getLecture2();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }
